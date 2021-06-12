9dced6058047fae983e5ac9f848f6a7cf18ede00
Anton Chaus <anchaus@c1r7p8.khpi.ucode-connect.study>
2021-06-12 14:01:20 +0300

    add seed for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'login' => "Admin",
+            'real_name' => Str::random(10),
+            'email' => "anton271993@ukr.net",
+            'password' => Hash::make('2222'),
+            'role' => "admin"
+        ]);
     }
 }
